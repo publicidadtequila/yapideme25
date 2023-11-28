3877b6aa179749e0b5ec085b4ed882d333e5e890
publicidadtequila <118697746+publicidadtequila@users.noreply.github.com>
2023-11-28 17:27:57 -0600

    redux changes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
 
-import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
 
 const persistConfig = {
   key: "sixam-mart",
